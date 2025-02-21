92298adf8576fbbe411f1d7c35e55c8ecd972990
Adolfo Baeza <adolfo.baeza@gmail.com>
2025-02-21 15:58:20 -0300

    Update dynamicCards.js

--- js/dynamicCards.js
+++ js/dynamicCards.js
@@ -1,27 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
     const imgFolder = "assets/img/";
-    const images = [
-        "dunalastair.jpeg",
-        "lincoln_college.png",
-        "palmares.jpeg",
-        "pumahue.jpeg",
-        "weston_academy.jpeg",
-    ]; // Esta lista se puede obtener dinámicamente desde un backend
+    
+    // Lista de imágenes con nombres personalizados
+    const images = [ 
+        { name: "Llavero Dunalastair", img: "dunalastair.jpeg" },
+        { name: "Llavero Lincoln College", img: "lincoln_college.jpeg" },
+        { name: "Llavero Palmares", img: "palmares.jpeg" },
+        { name: "Llavero Pumahue", img: "pumahue.jpeg" },
+        { name: "Llavero Weston Academy", img: "weston_academy.jpeg" }
+    ];
 
     const container = document.getElementById("productContainer");
 
-    images.forEach((img, index) => {
+    images.forEach((item) => {
         const card = document.createElement("div");
         card.className = "col mb-5";
 
         card.innerHTML = `
             <div class="card h-100">
                 <!-- Product image -->
-                <img class="card-img-top" src="${imgFolder + img}" alt="Llavero ${index + 1}" />
+                <img class="card-img-top" src="${imgFolder + item.img}" alt="${item.name}" />
                 <!-- Product details -->
                 <div class="card-body p-4">
                     <div class="text-center">
-                        <h5 class="fw-bolder">Llavero ${img}</h5>
+                        <h5 class="fw-bolder">${item.name}</h5>
                         <div class="d-flex justify-content-center small text-warning mb-2">
                             <div class="bi-star-fill"></div>
                             <div class="bi-star-fill"></div>
@@ -35,7 +37,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 </div>
                 <!-- Product actions -->
                 <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="https://wa.me/56951549184?text=Hola, Me gustaria un Llavero del colegio ${img}">Solicita vía Whatsapp</a></div>
+                    <div class="text-center"><a class="btn btn-outline-dark mt-auto" href="https://wa.me/56951549184?text=Hola, Me gustaria un ${item.name}">Solicita vía Whatsapp</a></div>
                 </div>
             </div>
         `;
